eac7bdf514caad33e8532f0a36be00976027e32c
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-29 11:56:30 +0000

    ifs and functions - probably a better way to do this:[

--- generate_blank_revtmd.py
+++ generate_blank_revtmd.py
@@ -7,8 +7,9 @@ from easygui import multenterbox, choicebox
 
 msg ="Which Workflow?"
 title = "Workflows"
-choices = ["Telecine One Light", "Telecine Bestlight", "Telecine Grade", "Tape Ingest 1", "Tape Ingest 2", "Tape Edit Suite 1", "Tape Ingest 2"]
+choices = ["Telecine One Light", "bestlight", "Telecine Grade", "Tape Ingest 1", "Tape Ingest 2", "Tape Edit Suite 1", "Tape Ingest 2"]
 choice = choicebox(msg, title, choices)
+print choice
 
 msg ="User?"
 title = "Pick a name yo!"
@@ -117,7 +118,13 @@ with open(inmagicxml, "w+") as fo:
     
 def add_to_revtmd(element, value, xmlfile):
     subprocess.call(['xml', 'ed', '--inplace', '-N', 'x=http://nwtssite.nwts.nara/schema/', '-u', element, '-v', value, xmlfile])
-    
-add_to_revtmd('//revtmd:filename', fieldValues[1], inmagicxml)
-add_to_revtmd('//revtmd:identifier', fieldValues[0], inmagicxml)
-add_to_revtmd('//revtmd:digitizationEngineer[1]', "Kieran O'Leary", inmagicxml)
+
+def bestlight():
+        
+    add_to_revtmd('//revtmd:filename', fieldValues[1], inmagicxml)
+    add_to_revtmd('//revtmd:identifier', fieldValues[0], inmagicxml)
+    add_to_revtmd('//revtmd:digitizationEngineer[1]', user, inmagicxml)
+
+if choice == "bestlight":
+    bestlight()
+
